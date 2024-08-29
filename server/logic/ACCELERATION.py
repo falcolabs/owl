6fbfb051e4e3bbb6b13569eeb5d8da165eae2c6c
Nguyễn Thế Thảo <nguyenthethaobnvn@gmail.com>
2024-08-29 11:27:34 +0700

    Update ACCELERATION.py
    Updating some question - related features and add timer.

--- server/logic/ACCELERATION.py
+++ server/logic/ACCELERATION.py
@@ -1,32 +1,49 @@
 
-import engine as BASE
-import penguin as CONF
+import engine
+import penguin
 import json
 import datetime as TIME
 
 #ACCELERATION PART
 
-Questions = {1:"Speed",2:"Organization",3:"Logic",4:"Identification"}
+Questions = {
+    1:"Speed",
+    2:"Organization",
+    3:"Logic",
+    4:"Identification"}
 
 RPC_object = penguin.rpc.RPCManager()
-Timing_object = engine.Timer()
-Timing_object.pause()
 
 #THE QUESTION DICTIONARY
 Question_storage_location = ""
 Question_repository = engine.QuestionBank.load(filepath = Question_storage_location)
 
+#THE TIME REFERENCE
+Time_reference = TIME.today()
+
+def Timer_start():
+    Start_time = Time_reference.second()
+
+def Timer_stop():
+    Stop_time = Time_reference.second()
+    Time_elapsed = Stop_time - Start_time
+    return Time_elapsed
+
 Question_number = 0
+Question_description = ""
 
 RPC.object.add_procedure(
     [
-        ("Answer", Timing_object.pause, []),
-        ("Start_timer", Timing_object.resume, []),
+        ("Answer", Timer_stop, []),
+        ("Start_timer", Timer_start, []),
         ("Proceed", engine.QuestionBank.get_question, [Question_number])
     ]
 )
 
-
+if Question_number == 0:
+    print(False)
+else:
+    Question_description = Questions[Question_number]
 '''
 TO BE DEVELOPED
 '''
