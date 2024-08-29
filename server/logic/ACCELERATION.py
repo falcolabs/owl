08a7aa3fc21d88cfb38fc272aa292e48740cff79
Nguyễn Thế Thảo <nguyenthethaobnvn@gmail.com>
2024-08-29 11:54:02 +0700

    Changing ACCELERATION.py
    Yes, yes, it is a bit wacky.

--- server/logic/ACCELERATION.py
+++ server/logic/ACCELERATION.py
@@ -1,36 +1,24 @@
 
-import engine
-import penguin
+import engine as BASE
+import penguin as CONF
 import json
 import datetime as TIME
 
 #ACCELERATION PART
 
-Questions = {
-    1:"Speed",
-    2:"Organization",
-    3:"Logic",
-    4:"Identification"}
+Questions = {1:"Speed",2:"Organization",3:"Logic",4:"Identification"}
 
 RPC_object = penguin.rpc.RPCManager()
-
-#THE QUESTION DICTIONARY
-Question_storage_location = ""
-Question_repository = engine.QuestionBank.load(filepath = Question_storage_location)
-
-#THE TIME REFERENCE
-Time_reference = TIME.today()
+Timing_object = TIME.today()
 
 def Timer_start():
-    Start_time = Time_reference.second()
+    Start_time = Timing_object.second()
 
 def Timer_stop():
-    Stop_time = Time_reference.second()
+    Stop_time = Timing_object.second()
     Time_elapsed = Stop_time - Start_time
-    return Time_elapsed
 
 Question_number = 0
-Question_description = ""
 
 RPC.object.add_procedure(
     [
@@ -40,10 +28,14 @@ RPC.object.add_procedure(
     ]
 )
 
+def Get_question(number: int):
+    return penguin.SHOW.qbank.get_question(number).prompt
+
 if Question_number == 0:
-    print(False)
+    False
 else:
-    Question_description = Questions[Question_number]
+    Get_question(Question_number)
+
 '''
 TO BE DEVELOPED
 '''
